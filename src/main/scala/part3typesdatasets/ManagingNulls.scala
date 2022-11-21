fba770689dd2836aa0477facd49dfc2dba885795
Žan Žibert <zzibert@plume.com>
2022-11-21 20:40:32 +0100

    Managing Nulls in Data

---
+++ src/main/scala/part3typesdatasets/ManagingNulls.scala
@@ -0,0 +1,52 @@
+package part3typesdatasets
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object ManagingNulls extends App {
+  val spark = SparkSession.builder()
+    .appName("Managing nulls")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  // select the first non-null value
+  moviesDF.select(
+    col("Title"),
+    col("Rotten_Tomatoes_Rating"),
+    col("IMDB_Rating") * 10,
+    coalesce(col("Rotten_Tomatoes_Rating"), col("IMDB_Rating") * 10)
+  )
+
+  // checking for nulls
+  moviesDF.select("*").where(col("Rotten_Tomatoes_Rating").isNotNull)
+
+  // nulls when ordering
+  moviesDF.orderBy(col("IMDB_Rating").desc_nulls_last)
+
+  // removing nulls
+  moviesDF.select("Title", "IMDB_Rating").na.drop() // remove rows containing nulls
+
+  // replace nulls
+  moviesDF.na.fill(0, List("IMDB_Rating", "Rotten_Tomatoes_Rating"))
+
+  moviesDF.na.fill(Map(
+    "IMDB_Rating" -> 0,
+    "Rotten_Tomatoes_Rating" -> 1,
+    "Director" -> "Unknown"
+  ))
+
+  // complex operations
+  moviesDF.selectExpr(
+    "Title",
+    "IMDB_Rating",
+    "Rotten_Tomatoes_Rating",
+    "ifnull(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as ifnull", // same as coalesce
+    "nvl(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as nvl",
+    "nullif(Rotten_Tomatoes_Rating, IMDB_Rating * 10) as nullif", // returns null if the two values are EQUAL, else first value
+    "nvl2(Rotten_Tomatoes_Rating, IMDB_Rating * 10, 0.0) as nvl2"
+  ).show()
+}
