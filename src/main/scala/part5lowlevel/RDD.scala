859759dcb6a4e11f1b7117541d36af5d96cb55f2
Žan Žibert <zzibert@plume.com>
2022-11-27 13:18:04 +0100

    RDDs, Part 2 + Exercise

--- src/main/scala/part5lowlevel/RDD.scala
+++ src/main/scala/part5lowlevel/RDD.scala
@@ -1,6 +1,6 @@
 package part5lowlevel
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{ SaveMode, SparkSession }
 import scala.io.Source
 
 object RDD extends App {
@@ -27,7 +27,7 @@ object RDD extends App {
       .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
       .toList
 
-  val stockRDD = sc.parallelize(readStocks("/src/main/resources/data/stocks.csv"))
+  val stocksRDD = sc.parallelize(readStocks("src/main/resources/data/stocks.csv"))
 
   // 2b - reading from files
   val stocksRDD2 = sc.textFile("src/main/resources/data/stocks.csv")
@@ -36,20 +36,103 @@ object RDD extends App {
     .map(tokens => StockValue(tokens(0), tokens(1), tokens(2).toDouble))
 
   // 3 - read from a DF
-  val stocksDF = spark.read
-    .option("header", "true")
-    .option("inferSchema", "true")
-    .csv("src/main/resources/data/stocks.csv")
-
+//  val stocksDF = spark.read
+//    .option("header", "true")
+//    .option("inferSchema", "true")
+//    .csv("src/main/resources/data/stocks.csv")
+//
   import spark.implicits._
-  val stocksDS = stocksDF.as[StockValue]
-  val stocksRDD3 = stocksDS.rdd
+//  val stocksDS = stocksDF.as[StockValue]
+//  val stocksRDD = stocksDS.rdd
 
-  val stocksRDD4 = stocksDF.rdd // lose type
+//  val stocksRDD4 = stocksDF.rdd // lose type
+//
+//  // RDD -> DF
+//  val numbersDF = numbersRDD.toDF("numbers") // you lose type info
+//
+//  // RDD -> DS
+//  val numbersDS = spark.createDataset(numbersRDD) //
+//
+//  // Transformation
+//
+  val msftRDD = stocksRDD.filter(_.company == "MSFT") // lazy transformation
 
-  // RDD -> DF
-  val numbersDF = numbersRDD.toDF("numbers") // you lose type info
+  val msftCount = msftRDD.count() // eager action
 
-  // RDD -> DS
-  val numbersDS = spark.createDataset(numbersRDD) // 
+  val companyNamesRDD = stocksRDD.map(_.company).distinct // lazy transformation
+
+  implicit val stockOrdering: Ordering[StockValue] = Ordering.fromLessThan((a, b) => a.price < b.price)
+
+  val minMsft = msftRDD.min() // action
+
+  // reduce
+//  println(numbersRDD.reduce((a, b) => a + b))
+
+  // grouping
+  val groupedStocksRDD = stocksRDD.groupBy(_.company)
+
+//  groupedStocksRDD foreach { tuple =>
+//    println(s"${tuple._1}, ${tuple._2.maxBy(_.price)} ") // shuffling
+//  }
+
+  // partitioning
+  val repartitionedStocksRDD = stocksRDD.repartition(30)
+
+//  repartitionedStocksRDD.toDF.write
+//    .mode(SaveMode.Overwrite)
+//    .parquet("src/main/resources/data/stocks30")
+
+  // repartitioning is expensive. Involves shuffling
+  /*
+  * Size of a partition 10-100MB
+  * */
+
+
+  // coalasce
+  val coalascedRDD = repartitionedStocksRDD.coalesce(15) // does not necessarily involve shuffling
+
+//  coalascedRDD.toDF.write
+//    .mode(SaveMode.Overwrite)
+//    .parquet("src/main/resources/data/stocks30")
+
+  /*
+
+  2.
+   */
+
+  case class Movie(Title: Option[String], Major_Genre: Option[String], IMDB_Rating: Option[Double])
+
+
+
+
+
+
+  // 1. read the movies.json as an RDD
+  import spark.implicits._
+  val moviesRDD = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+    .as[Movie]
+    .rdd
+
+  // 2. show the distinct genres as an RDD
+  val distinctGenresRDD = moviesRDD.flatMap(_.Major_Genre).distinct()
+
+//  distinctGenresRDD.foreach(println)
+
+  // 3. select all the movies in the Drama genre with iMDB rating > 6
+  val goodDramasRDD = moviesRDD.filter(movie => movie.Major_Genre.contains("Drama")  && movie.IMDB_Rating.exists(_ > 6))
+
+//  goodDramasRDD.foreach(println)
+
+  // 5. show the avg rating of movies by genre
+  val avgRatingByGenreRDD = moviesRDD.groupBy(_.Major_Genre) foreach { tuple =>
+    println(s"genre : ${tuple._1} avg rating: ${getAvgRating(tuple._2)}")
+  }
+
+  def getAvgRating(movies: Iterable[RDD.Movie]): Double = {
+    movies.flatMap(_.IMDB_Rating).reduce((a, b) => a + b) / movies.size
+  }
+
+
 }
